d20a2e8e53000939b607e0e8f405c58764c9a95b
oayoahz <oayoahz@gmail.com>
2023-12-02 16:33:39 +0800

    update settings

--- config.fish
+++ config.fish
@@ -1,3 +1,52 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+set fish_greeting ""
+
+set -gx TERM xterm-256color
+
+# theme
+set -g theme_color_scheme terminal-dark
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
+
+# aliases
+
+alias ls "eza --icons"
+alias la "eza -a --icons"
+alias ll "eza -l -g --icons"
+alias lla "ll -a"
+alias g git
+alias snvim sudoedit
+command -qv nvim && alias vim nvim
+
+set -gx EDITOR nvim
+
+set -gx PATH bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH
+
+# NodeJS
+set -gx PATH node_modules/.bin $PATH
+
+# Go
+set -g GOPATH $HOME/go
+set -gx PATH $GOPATH/bin $PATH
+
+# NVM
+function __check_rvm --on-variable PWD --description 'Do nvm stuff'
+    status --is-command-substitution; and return
+
+    if test -f .nvmrc; and test -r .nvmrc
+        nvm use
+    else
+    end
+end
+
+set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
+if test -f $LOCAL_CONFIG
+    source $LOCAL_CONFIG
+end
