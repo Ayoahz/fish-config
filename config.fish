130ff1daaae1705a35e72d4a79a90d2ef087330f
Ayoahz <oayoahz@gmail.com>
2023-12-07 01:10:07 +0800

    delete tide

--- config.fish
+++ config.fish
@@ -10,21 +10,31 @@ set -gx TERM xterm-256color
 set -g theme_color_scheme terminal-dark
 set -g fish_prompt_pwd_dir_length 1
 set -g theme_display_user yes
-set -g theme_hide_hostname no
+set -g theme_hide_hostname yes
 set -g theme_hostname always
 
 # aliases
-
-alias ls "eza --icons"
-alias la "eza -a --icons"
-alias ll "eza -l -g --icons"
-alias lla "ll -a"
+## list
+alias ls "eza --icons --sort type"
+alias la "eza -l -g -a --icons --sort type"
+alias ll "eza -l -g --icons --sort type"
+alias lla "ll -a --sort type"
+alias lz "eza --tree --icons --sort type"
+alias lza "eza --tree -a --icons --sort type"
+## git
 alias g git
-alias snvim sudoedit
+alias gs "git status"
+alias gc "git commit"
+alias gd "git diff"
+alias gp "git push"
+## sudoedit
+alias svim sudoedit
 command -qv nvim && alias vim nvim
 
+# EDITOR
 set -gx EDITOR nvim
 
+# PATH
 set -gx PATH bin $PATH
 set -gx PATH ~/bin $PATH
 set -gx PATH ~/.local/bin $PATH
@@ -35,6 +45,9 @@ set -gx PATH node_modules/.bin $PATH
 # Go
 set -g GOPATH $HOME/go
 set -gx PATH $GOPATH/bin $PATH
+
+# GHQ
+set -gx GHQ_ROOT $HOME/repo
 
 # NVM
 function __check_rvm --on-variable PWD --description 'Do nvm stuff'
